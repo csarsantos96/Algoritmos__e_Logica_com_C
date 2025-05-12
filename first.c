ab7e8e6756738f8c7b892ca82e73f17a43c169d3
Cesar Santos <csar.santos18@gmail.com>
2025-05-12 16:28:44 -0500

    atualização

---
+++ first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 10;
+    float numQuebrado = 50.49;
+
+    printf("Numero: %d\n", numero);
+
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include <stdio.h> // stdio.h -> é uma lib // | std -> standart -> padrão | i -> input -> entrada | o -> output -> saída.
 
-int main(void)
-{
-    printf("Hello, World!\n");
+int main(){
+    printf("Hello World");
+
+    // printf = saída //
+
     return 0;
 }
+
+
+
+
+
+
+
+
+// int -> tipos de dados
